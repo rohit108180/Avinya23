0009b124d40666a12ab548c6d3cc5b42f9f5bf41
Rohit Sharma <72381583+rohit108180@users.noreply.github.com>
2023-02-17 21:39:16 +0530

    merged

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from "react";
+import Banner from "./components/banner/Banner";
+import Description from "./components/description/Description";
 import Gallery from "./components/Gallery";
-import Events from "./components/Events/Events"
+import Events from "./components/Events/Events";
 import InitialLoader from "./components/Loader/InitialLoader";
 import Participate from "./components/Participate";
 import Footer from "./components/Footer"
 import Discord from "./components/Dicord"
 import Sponsors from "./components/timelineSponsors/sponsors/sponsors"
 import Timeline from "./components/timelineSponsors/timeline/timelinediv"
+import Footer from "./components/Footer";
+import Discord from "./components/Dicord";
+import Navbar from "./components/navbar/Navbar"
 
 function App() {
+    const [loading, setLoading] = useState(false);
 
-  const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 3500);
+    }, []);
 
   useEffect(() => {
     setLoading(true);
@@ -20,25 +31,26 @@ function App() {
   }, [])
 
   return (
-    <>
-      {
-        loading ? (<InitialLoader />) :
-          (
-            <>
-              <Participate />
-              <Events/>
-              <Gallery />
-              <Timeline/>
-              <Sponsors/>
-              <Discord/>
-              <Footer/>
-            </>
-          )
-      }
 
-
-    </>
-  );
+        <>
+            {loading ? (
+                <InitialLoader />
+            ) : (
+                <>
+                    <Navbar />
+                    <Banner />
+                    <Description />
+                    <Participate />
+                    <Gallery/>
+                    <Timeline/>
+                    <Events />
+                    <Discord />
+                    <Sponsors/>
+                    <Footer />
+                </>
+            )}
+        </>
+    );
 }
 
 export default App;
