8bea71753398a416c2bdb926eec1e61b54a79e78
Rohit Sharma <72381583+rohit108180@users.noreply.github.com>
2023-02-17 21:50:57 +0530

    merged

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from "react";
+import InitialLoader from "./components/Loader/InitialLoader";
+import Navbar from "./components/navbar/Navbar"
 import Banner from "./components/banner/Banner";
 import Description from "./components/description/Description";
 import Gallery from "./components/Gallery";
-import Events from "./components/Events/Events";
-import InitialLoader from "./components/Loader/InitialLoader";
 import Participate from "./components/Participate";
-import Footer from "./components/Footer"
+import Events from "./components/Events/Events";
+import Timeline from "./components/timelineSponsors/timeline/timelinediv"
 import Discord from "./components/Dicord"
 import Sponsors from "./components/timelineSponsors/sponsors/sponsors"
-import Timeline from "./components/timelineSponsors/timeline/timelinediv"
 import Footer from "./components/Footer";
-import Discord from "./components/Dicord";
-import Navbar from "./components/navbar/Navbar"
+
 
 function App() {
     const [loading, setLoading] = useState(false);
