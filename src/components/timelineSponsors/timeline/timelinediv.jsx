85994b9e948cdaa99eccfd883357fe885fd8ffa7
Chirag <chiragsaini6405@gmail.com>
2023-02-21 14:21:58 +0530

    added 5 day timeline

--- src/components/timelineSponsors/timeline/timelinediv.jsx
+++ src/components/timelineSponsors/timeline/timelinediv.jsx
@@ -79,6 +79,48 @@ function Timelinediv() {
                 name: "Event 12",
                 date: "13 March 2022, Wednesday"
             }
+        ], [        // day 3
+            {
+                id: "01",
+                name: "Event 14",
+                date: "13 March 2022, Saturday"
+            },
+            {
+                id: "02",
+                name: "Event 10",
+                date: "13 March 2022, Monday"
+            },
+            {
+                id: "03",
+                name: "Event 11",
+                date: "13 March 2022, Tuesday"
+            },
+            {
+                id: "04",
+                name: "Event 12",
+                date: "13 March 2022, Wednesday"
+            }
+        ], [        // day 3
+            {
+                id: "01",
+                name: "Event 18",
+                date: "13 March 2022, Saturday"
+            },
+            {
+                id: "02",
+                name: "Event 10",
+                date: "13 March 2022, Monday"
+            },
+            {
+                id: "03",
+                name: "Event 11",
+                date: "13 March 2022, Tuesday"
+            },
+            {
+                id: "04",
+                name: "Event 12",
+                date: "13 March 2022, Wednesday"
+            }
         ]
     ]
 
@@ -86,6 +128,8 @@ function Timelinediv() {
     const [day1, setday1] = useState('block');
     const [day2, setday2] = useState('none');
     const [day3, setday3] = useState('none');
+    const [day4, setday4] = useState('none');
+    const [day5, setday5] = useState('none');
 
     var change = (e, btnnm) => {
         if (btnnm === 1) {
@@ -93,18 +137,40 @@ function Timelinediv() {
             setday1("block")
             setday2("none")
             setday3("none")
+            setday4("none")
+            setday5("none")
         }
         else if (btnnm === 2) {
             setActiveButton("Button 2");
             setday2("block")
             setday1("none")
             setday3("none")
+            setday4("none")
+            setday5("none")
         }
         else if (btnnm === 3) {
             setActiveButton("Button 3");
             setday3("block")
             setday2("none")
             setday1("none")
+            setday4("none")
+            setday5("none")
+        }
+        else if (btnnm === 4) {
+            setActiveButton("Button 4");
+            setday4("block")
+            setday2("none")
+            setday1("none")
+            setday3("none")
+            setday5("none")
+        }
+        else if (btnnm === 5) {
+            setActiveButton("Button 5");
+            setday5("block")
+            setday2("none")
+            setday1("none")
+            setday4("none")
+            setday3("none")
         }
     }
     return (
@@ -113,31 +179,46 @@ function Timelinediv() {
                 <section>
                     <div class="container">
                         {/* <h1 class="title">EVENT SCHEDULES</h1> */}
-                        <Heading name = "EVENT SCHEDULES"/>
+                        <Heading name="EVENT SCHEDULES" />
                         <div class="">
-                            <button class="timeline-btn" style={activeButton === "Button 1" ? { backgroundColor: "transparent", color: "#4120a8", borderColor: "#4120a8", borderTopRightRadius: "10px", borderTopLeftRadius: "10px",  } : {}} onClick={(e) => { change(e, 1) }}>
+                            <button class="timeline-btn" style={activeButton === "Button 1" ? { backgroundColor: "transparent", color: "#4120a8", borderColor: "#4120a8", borderTopRightRadius: "10px", borderTopLeftRadius: "10px", } : {}} onClick={(e) => { change(e, 1) }}>
                                 DAY 1
                             </button>
-                            <button class="timeline-btn" style={activeButton === "Button 2" ? { backgroundColor: "transparent", color: "#4120a8", borderColor: "#4120a8", borderTopRightRadius: "10px", borderTopLeftRadius: "10px",  } : {}} onClick={(e) => { change(e, 2) }}>
+                            <button class="timeline-btn" style={activeButton === "Button 2" ? { backgroundColor: "transparent", color: "#4120a8", borderColor: "#4120a8", borderTopRightRadius: "10px", borderTopLeftRadius: "10px", } : {}} onClick={(e) => { change(e, 2) }}>
                                 DAY 2
                             </button>
-                            <button class="timeline-btn" style={activeButton === "Button 3" ? { backgroundColor: "transparent", color: "#4120a8", borderColor: "#4120a8", borderTopRightRadius: "10px", borderTopLeftRadius: "10px",  } : {}} onClick={(e) => { change(e, 3) }}>
+                            <button class="timeline-btn" style={activeButton === "Button 3" ? { backgroundColor: "transparent", color: "#4120a8", borderColor: "#4120a8", borderTopRightRadius: "10px", borderTopLeftRadius: "10px", } : {}} onClick={(e) => { change(e, 3) }}>
                                 DAY 3
+                            </button>
+                            <button class="timeline-btn" style={activeButton === "Button 4" ? { backgroundColor: "transparent", color: "#4120a8", borderColor: "#4120a8", borderTopRightRadius: "10px", borderTopLeftRadius: "10px", } : {}} onClick={(e) => { change(e, 4) }}>
+                                DAY 4
+                            </button>
+                            <button class="timeline-btn" style={activeButton === "Button 5" ? { backgroundColor: "transparent", color: "#4120a8", borderColor: "#4120a8", borderTopRightRadius: "10px", borderTopLeftRadius: "10px", } : {}} onClick={(e) => { change(e, 5) }}>
+                                DAY 5
                             </button>
                         </div>
                         <div className="timeline-combined">
-                        <div class='day1info text-center' style={{ "display": day1 }} data-aos="zoom-in" >
-                            {/* <h1 class='text-3xl font-bold m-20 block'>Day1 info</h1> */}
-                            {timeline[0].map((detail, index) => <Timeline key={index} timeline={index} items={detail} />)}
-                        </div>
-                        <div class='day2info text-center' style={{ "display": day2 }} data-aos="zoom-in">
-                            {/* <h1 class='text-3xl font-bold m-20 block'>Day2 info</h1> */}
-                            {timeline[1].map((detail, index) => <Timeline key={index} timeline={index} items={detail} />)}
-                        </div>
-                        <div class='day3info text-center' style={{ "display": day3 }} data-aos="zoom-in">
-                            {/* <h1 class='text-3xl font-bold m-20 block'>Day3 info</h1> */}
-                            {timeline[2].map((detail, index) => <Timeline key={index} timeline={index} items={detail} />)}
-                        </div>
+                            <p style={{ "font-size": "30px" }}>To be announced soon!</p>
+                            {/* <div class='day1info text-center' style={{ "display": day1 }} data-aos="zoom-in" >
+                                <h1 class='text-3xl font-bold m-20 block'>Day1 info</h1>
+                                {timeline[0].map((detail, index) => <Timeline key={index} timeline={index} items={detail} />)}
+                            </div>
+                            <div class='day2info text-center' style={{ "display": day2 }} data-aos="zoom-in">
+                                <h1 class='text-3xl font-bold m-20 block'>Day2 info</h1>
+                                {timeline[1].map((detail, index) => <Timeline key={index} timeline={index} items={detail} />)}
+                            </div>
+                            <div class='day3info text-center' style={{ "display": day3 }} data-aos="zoom-in">
+                                <h1 class='text-3xl font-bold m-20 block'>Day3 info</h1>
+                                {timeline[2].map((detail, index) => <Timeline key={index} timeline={index} items={detail} />)}
+                            </div>
+                            <div class='day4info text-center' style={{ "display": day4 }} data-aos="zoom-in">
+                                <h1 class='text-3xl font-bold m-20 block'>Day4 info</h1>
+                                {timeline[3].map((detail, index) => <Timeline key={index} timeline={index} items={detail} />)}
+                            </div>
+                            <div class='day5info text-center' style={{ "display": day5 }} data-aos="zoom-in">
+                                <h1 class='text-3xl font-bold m-20 block'>Day5 info</h1>
+                                {timeline[4].map((detail, index) => <Timeline key={index} timeline={index} items={detail} />)}
+                            </div> */}
                         </div>
                     </div>
                 </section>
