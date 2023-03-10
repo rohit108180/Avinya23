f349aec93a95842dfd5107f416e70060806f1db1
Bhavesh-Gihar <97470501+Bhavesh-Gihar@users.noreply.github.com>
2023-03-11 00:09:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,19 @@
-import React, { useEffect, useState } from "react";
-import InitialLoader from "./components/Loader/InitialLoader";
-import Navbar from "./components/navbar/Navbar"
-import Banner from "./components/banner/Banner";
-import Description from "./components/description/Description";
-import Gallery from "./components/Gallery";
-import Participate from "./components/Participate";
-import Timeline from "./components/timelineSponsors/timeline/timelinediv"
-import Discord from "./components/Dicord"
-import Sponsors from "./components/timelineSponsors/sponsors/sponsors"
-import Footer from "./components/Footer";
-import Heading from "./components/Heading/Heading";
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-
-
+import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
+import Home from "./components/Home.js";
+import TeamPage from "./components/TeamPage.js";
 
 
 function App() {
-    useEffect(()=>{
-        AOS.init({duration:1500});
-    },[]);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-        }, 4800);
-    }, []);
-
 
   return (
-
-        <>
-            {loading ? (
-                <InitialLoader />
-            ) : (
-                <>
-                    <Navbar />
-                    <Banner />
-                    <Description />
-                    <Participate />
-                    <Gallery/>
-                    <Timeline/>
-                    <Discord />
-                    <Sponsors/>
-                    <Footer />
-                </>
-            )}
-        </>
+    <>
+    <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/team" element={<TeamPage/>}/>
+        </Routes>
+    </Router>
+  </>
     );
 }
 
