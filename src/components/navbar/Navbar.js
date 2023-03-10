8ad95afc374b2c4f575fc53b89da767638901bbb
Ayush Goyal <93733866+Ayush-207@users.noreply.github.com>
2023-03-10 22:41:41 +0530

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import MenuWrap from '../menu-wrap/MenuWrap'
+import TeamMembers from '../TeamMembers.js'
 import './Navbar.css'
 
 export default function Navbar() {
@@ -11,6 +12,7 @@ export default function Navbar() {
         <a className='nav-a' href="#schedules"><div className='nav-item'>SCHEDULES</div></a>
         <a className='nav-a' href="#gallery"><div className='nav-item'>GALLERY</div></a>
         <a className='nav-a' href="#sponsors"><div className='nav-item'>SPONSORS</div></a>
+        <a className='nav-a' href="#team"><TeamMembers/>TEAM</a>
         <a className='nav-a' href="/"><div className='nav-item proj-button'>PROJECTS</div></a>
     </nav>
     <nav className="nav-2">
