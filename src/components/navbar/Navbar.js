eea2b864b87de307c71e7d56e6ba18026eedc8bc
Bhavesh-Gihar <97470501+Bhavesh-Gihar@users.noreply.github.com>
2023-03-11 00:42:08 +0530

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,10 +8,10 @@ export default function Navbar() {
     <>
     <nav className='nav'>
         <a className='nav-a' href="/"><div className='nav-item'>HOME</div></a>
-        <a className='nav-a' href="#events"><div className='nav-item'>EVENTS</div></a>
-        <a className='nav-a' href="#schedules"><div className='nav-item'>SCHEDULES</div></a>
-        <a className='nav-a' href="#gallery"><div className='nav-item'>GALLERY</div></a>
-        <a className='nav-a' href="#sponsors"><div className='nav-item'>SPONSORS</div></a>
+        <a className='nav-a' href="/#events"><div className='nav-item'>EVENTS</div></a>
+        <a className='nav-a' href="/#schedules"><div className='nav-item'>SCHEDULES</div></a>
+        <a className='nav-a' href="/#gallery"><div className='nav-item'>GALLERY</div></a>
+        <a className='nav-a' href="/#sponsors"><div className='nav-item'>SPONSORS</div></a>
         <Link className='nav-a' to="/team" style={{marginLeft: "2rem"}}>TEAM</Link>
         <a className='nav-a' href="/"><div className='nav-item proj-button'>PROJECTS</div></a>
     </nav>
@@ -20,3 +20,4 @@ export default function Navbar() {
     </>
   )
 }
+
